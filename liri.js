1635352e862cd20789d823b32716904b24578c30
Yegor B <biryuky1@tcnj.edu>
2019-08-25 21:39:10 -0400

    finished liri hw

--- liri.js
+++ liri.js
@@ -1,3 +1,9 @@
+// node liri.js concert-this twenty one pilots
+// node liri.js spotify-this-song stacys mom
+// node liri.js movie-this troy
+// node liri.js movie-this
+// node liri.js do-what-it-says
+
 require("dotenv").config();
 
 var keys = require("./keys.js");
@@ -7,22 +13,6 @@ var Spotify = require("node-spotify-api");
 var fs = require("fs")
 
 var spotify = new Spotify(keys.spotify);
-
-// commands
-// * `concert-this`
-// 1. `node liri.js concert-this <artist/band name here>`
-
-
-// * `spotify-this-song`
-// 2. `node liri.js spotify-this-song '<song name here>'`
-
-
-// * `movie-this`
-// 3. `node liri.js movie-this '<movie name here>'`
-
-
-// * `do-what-it-says`
-// 4. `node liri.js do-what-it-says`
 
 var typeOfRequest = process.argv[2]
 var nameToSearch = ""
@@ -34,24 +24,81 @@ for (i = 3; i < process.argv.length; i++) {
 
 nameToSearch = nameToSearch.substring(0, nameToSearch.length - 1)
 
-console.log("*" + nameToSearch + "*")
+// console.log("*" + nameToSearch + "*")
 
 if (typeOfRequest == "concert-this") {
-    printConcertInfo()
+    PrintConcertInfo()
 } else if (typeOfRequest == "spotify-this-song") {
-
+    PrintSongInfo()
 } else if (typeOfRequest == "movie-this") {
-    printMovieInfo()
+    PrintMovieInfo()
 } else if (typeOfRequest == "do-what-it-says") {
-
+    ReadFromTextFile()
 } else {
     console.log("Improper request type. Valid request types are \"concert-this\", \"spotify-this-song\", \"movie-this\", or \"do-what-it-says\"")
     process.exit()
 }
 
-console.log(URL)
 
-function printConcertInfo() {
+function ReadFromTextFile() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // We will then print the contents of data
+        console.log(data);
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+
+        // We will then re-display the content as an array for later use.
+        var reqType = dataArr[0]
+        nameToSearch = dataArr[1]
+
+        nameToSearch = NewReplace(nameToSearch, " ", "+").trim()
+
+        if (reqType == "concert-this") {
+            PrintConcertInfo()
+        } else if (reqType == "spotify-this-song") {
+            PrintSongInfo()
+        } else if (reqType == "movie-this") {
+            PrintMovieInfo()
+        } else {
+            console.log("Invalid request type in text file.")
+        }
+    });
+}
+
+
+
+function PrintSongInfo() {
+    
+    var myLimit = 1
+
+    // spotify responses are too annoying to search, sorry ):
+    //
+    // if (nameToSearch == ""){
+    //     nameToSearch = "The Sign"
+    //     myLimit = 5
+    // }
+    
+    spotify.search({ type: "track", query: nameToSearch, limit: myLimit }, function (error, data) {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log("Artists: " + data.tracks.items[0].artists[0].name)
+            console.log("Song Name: " + data.tracks.items[0].name);
+            console.log("Preview Link: " + data.tracks.items[0].preview_url);
+            console.log("Album: " + data.tracks.items[0].album.name);
+        }
+    })
+}
+
+
+function PrintConcertInfo() {
     var eventNum = 1
     URL = "https://rest.bandsintown.com/artists/" + nameToSearch + "/events?app_id=codingbootcamp"
 
@@ -67,7 +114,8 @@ function printConcertInfo() {
     })
 }
 
-function printMovieInfo() {
+
+function PrintMovieInfo() {
     if (nameToSearch == "") {
         nameToSearch = "Mr. Nobody"
     }
@@ -92,9 +140,7 @@ function printMovieInfo() {
     )
 }
 
+
 function NewReplace(string, replace, replaceWith) {
     return string.split(replace).join(replaceWith)
 }
-
-
-
